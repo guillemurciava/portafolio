1bbac699797f74a347d2e407881c0a91bc27615f
“Guillermo <guillemurcia@gmail.com>
2024-12-02 23:39:45 -0600

    segunda carga de Portafolio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Reoutes, Reoute, Route, Routes} from 'react-router-dom'; 
-import Navbar from './Navbar';
-import Footer from './Footer';
-import Home from './Home';
-import About from './About';
-import Projects from './Projects';
-import Contact from './Contact';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 import './App.css';
 
 function App() {
